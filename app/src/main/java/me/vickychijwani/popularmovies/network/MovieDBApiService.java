14cdf8c312de7be94a244da903e71c236b73dbe0
Vicky Chijwani <vickychijwani@gmail.com>
2015-12-13 22:55:07 +0530

    code style changes.

--- app/src/main/java/me/vickychijwani/popularmovies/network/MovieDBApiService.java
+++ app/src/main/java/me/vickychijwani/popularmovies/network/MovieDBApiService.java
@@ -5,7 +5,7 @@ import retrofit.Call;
 import retrofit.http.GET;
 import retrofit.http.Query;
 
-public interface MovieDBApiService {
+interface MovieDBApiService {
 
     @GET("discover/movie?vote_count.gte=250")
     Call<MovieResults> fetchMovies(@Query("api_key") String apiKey, @Query("sort_by") String sortBy);
